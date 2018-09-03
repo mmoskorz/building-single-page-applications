f758dae4331fe6c62f7cb8523f3980c3bd541866
Marcos Moskorz <mmoskorz@gmail.com>
2018-09-03 18:05:39 -0300

    Start use of Vue Router

---
+++ Vue/alurapic/alurapic/src/components/home/Home.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
+
+    <ul class="lista-fotos">
+
+      <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
+
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+        </meu-painel>
+
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  import Painel from './shared/painel/Painel.vue';
+  import ImagemResponsiva from './shared/imagem-responsiva/ImagemResponsiva.vue'
+
+  export default {
+    name: 'app',
+
+    components: {
+
+      'meu-painel': Painel,
+      'imagem-responsiva': ImagemResponsiva
+    },
+
+    data() {
+      return {
+        titulo: 'Alurapic',
+        fotos: [],
+        filtro: ''
+      }
+    },
+    computed: {
+
+      fotosComFiltro() {
+
+        if (this.filtro) {
+
+          let exp = new RegExp(this.filtro.trim(), 'i');
+          return this.fotos.filter(foto => exp.test(foto.titulo));
+        } else {
+
+          return this.fotos;
+        }
+      }
+
+    },
+    created() {
+      this.$http.get('http://localhost:3000/v1/fotos')
+        .then(res => res.json())
+        .then(fotos => this.fotos = fotos, err => console.log(err));
+    }
+  }
+</script>
+
+<style>
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+    .lista-fotos .lista-fotos-item {
+      display: inline-block;
+    }
+
+  .titulo {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+    .lista-fotos .lista-fotos-item {
+      display: inline-block;
+    }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+</style>
