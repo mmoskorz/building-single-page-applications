6fe5cb79e1532275305dc4c417d9bb6bd1156e2b
Marcos Moskorz <mmoskorz@gmail.com>
2018-09-03 16:29:50 -0300

    Created a shared component

--- Vue/alurapic/alurapic/src/App.vue
+++ Vue/alurapic/alurapic/src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo" />
+  <div class="corpo">
+
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+
+      <li class="lista-fotos-item" v-for="foto in fotos">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
       </li>
     </ul>
+
   </div>
 </template>
 
 <script>
+  import Painel from './components/shared/painel/Painel.vue';
+
   export default {
     name: 'app',
+
+    components: {
+
+      'meu-painel': Painel
+    },
+
     data() {
       return {
         titulo: 'Alurapic',
@@ -27,4 +43,43 @@
 </script>
 
 <style>
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .titulo {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
